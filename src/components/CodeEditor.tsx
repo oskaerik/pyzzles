557fa3e1bebbf3c3d412a14d657fc5a885656690
Oskar Eriksson <oskaerik@gmail.com>
2024-11-10 07:39:33 +0100

    Fix editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -21,7 +21,7 @@ const CodeEditor = ({ value, onChange, readOnly = false, className }: CodeEditor
           minimap: { enabled: false },
           fontSize: 14,
           lineNumbers: "on",
-          lineNumbersMinChars: 2,
+          lineNumbersMinChars: 3,
           readOnly,
           scrollBeyondLastLine: false,
           automaticLayout: true,
