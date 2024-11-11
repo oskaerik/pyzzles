e713c118fe1feb310f08d996b82459a5f67f0158
Oskar Eriksson <oskaerik@gmail.com>
2024-11-11 20:48:00 +0100

    Add iterint

--- src/puzzles/iterint/solution.py
+++ src/puzzles/iterint/solution.py
@@ -5,7 +5,7 @@ orig = isinstance
 def patch(*args, **kwargs):
     caller = currentframe().f_back
     if caller.f_code.co_name == "test":
-        caller.f_globals["x"] = [1, 2, 3] 
+        caller.f_globals["x"] = [1, 2, 3]
     return orig(*args, **kwargs)
 
 globals()["__builtins__"]["isinstance"] = patch
