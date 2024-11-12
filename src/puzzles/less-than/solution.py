dc0ef0799507175381aa3924aa8574095cea9024
Oskar Eriksson <oskaerik@gmail.com>
2024-11-12 13:06:00 +0100

    Add less-than

---
+++ src/puzzles/less-than/solution.py
@@ -0,0 +1,6 @@
+import sys
+
+m = sys.float_info.mant_dig
+
+# https://blog.codingconfessions.com/p/how-python-compares-floats-and-ints
+x = 2**m + 1
---
+++ src/puzzles/less-than/test_solution.py
@@ -0,0 +1,6 @@
+"""solvable without custom types (kudos Udzu)"""
+
+def test():
+    from solution import x
+
+    assert x + 1.0 < x
